275b9af598d364f405b33d4c8bb328e02fb4ed04
RafaelBorges22 <rafaelmascarenhasborges@gmail.com>
2025-09-01 11:08:56 -0300

    Criação de componentes e estilização com CSS puro :white_check_mark:

--- src/main/resources/templates/projectLdwFront/src/App.vue
+++ src/main/resources/templates/projectLdwFront/src/App.vue
@@ -1,14 +1,41 @@
-<script setup>
-import './assets/main.css'
-import NavBar from './components/global/NavBar.vue';
-import BodyHome from './components/home/BodyHome.vue';
-import Footer from './components/global/Footer.vue';
-</script>
-
 <template>
-  <NavBar />
-  <BodyHome />
-  <Footer />
+  <div id="app">
+    <Navbar />
+    <HeroSection />
+
+    <AboutSection
+      title="SOBRE TATUAGENS"
+      text="lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      imageUrl="../../../public/images/photos/AboutImgLast.png" />
+
+    <AboutSection
+      title="SOBRE MIM"
+      text="lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      imageUrl="../../../public/images/photos/AboutImgFirst.png" reverse
+    />
+    
+    <Footer />
+
+  </div>
 </template>
 
+<script>
+import Navbar from './components/global/Navbar.vue';
+import HeroSection from './components/home/HeroSection.vue';
+import AboutSection from './components/home/AboutSection.vue';
+import Footer from './components/global/Footer.vue';
 
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    HeroSection,
+    AboutSection, 
+    Footer
+  }
+};
+</script>
+
+<style>
+@import './assets/Global.css';
+</style>
---
+++ src/main/resources/templates/projectLdwFront/src/components/home/AboutSection.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="about-section" :class="{ 'reverse-layout': reverse }">
+    <div class="about-image-container">
+      <img :src="imageUrl" alt="About Image" class="about-image" />
+    </div>
+    <div class="about-content">
+      <h2 class="about-title">{{ title }}</h2>
+      <p class="about-text">{{ text }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AboutSection',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    imageUrl: {
+      type: String,
+      required: true
+    },
+    reverse: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style scoped>
+@import '../../assets/home/AboutSection.css';
+</style>
