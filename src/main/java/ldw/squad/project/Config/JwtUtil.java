18ff8d804f07c9c14a84e1dd0f5e5b5ed1f094f4
Kevyn-Cavalcanti <contacontaconta0002@gmail.com>
2025-10-12 20:25:41 -0300

    mudança token

--- src/main/java/ldw/squad/project/Config/JwtUtil.java
+++ src/main/java/ldw/squad/project/Config/JwtUtil.java
@@ -19,7 +19,7 @@ public class JwtUtil {
         return Jwts.builder()
                 .setSubject(username)
                 .claim("role", role)
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
                 .signWith(key)
                 .compact();
     }
